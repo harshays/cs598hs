2375c57c297de1ce52a14b1459191832c29ae70e
Haurya <achung13@illinois.edu>
2017-11-07 21:48:40 -0800

    alter_structure implementation

--- run.py
+++ run.py
@@ -26,7 +26,32 @@ def initialize_graph(g, lambda_read, lambda_post):
 def compute_resource_utilization(g):
     raise NotImplementedError
 
-def alter_structure(g):
+def alter_structure(g, m):
+    """
+    We alter the graph structure to simulate creation of friendships and observe changes in B over time as a consequence.
+    m = float, arbitrary fraction of nodes
+    g = original graph 
+
+    Naive implementation - generates num_nodes*num_nodes tuples and samples (m*num_nodes)/2 tuples randomly
+                           no duplicates or self-loops
+
+    """
+
+    possible_friendships = [(i,j) for i in xrange(num_nodes) for j in xrange(i,num_nodes) if i != j]
+    #g.add_Edges(random.sample(possible_friendships, (m*num_nodes)/2)) #doesn't work because this doesn't consider edges that are already in the graph
+    edges = [(node.source, node.target) for node in g.es]
+    counter = 0
+    while counter < (m*num_nodes)/2:
+        candidate_edge = random.choice(possible_friendships)
+        if candidate_edge not in edges:
+            counter += 1
+            g.add_Edges([candidate_edge])
+
+    return g
+
+
+
+
     raise NotImplementedError
 
 def update(g):
